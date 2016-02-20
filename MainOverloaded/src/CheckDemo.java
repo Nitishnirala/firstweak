fa9c735fc5f83940e24f8d75ef9853b7f68a12a8
Nitishnirala <nitish.nirala@a-cti.com>
2016-02-20 18:39:49 +0530

    main method overloading

---
+++ MainOverloaded/src/CheckDemo.java
@@ -0,0 +1,15 @@
+
+public class CheckDemo {
+
+	String name="nitish";
+	int i=30;
+	public void process(CheckDemo d){
+		System.out.println();
+	}
+	public static void main(String[] args) {
+		
+		CheckDemo ch=new CheckDemo();
+		
+		System.out.println(ch.name);
+	}
+}
